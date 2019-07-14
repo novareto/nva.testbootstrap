9a9d6d5a2e7664eb60a7a4b17b84af2166acf686
Lars Walther <meinkraeks@gmail.com>
2019-07-14 12:37:59 +0200

    bootstrap content überarbeitet

--- src/nva/testbootstrap/views/bootstrap_content.py
+++ src/nva/testbootstrap/views/bootstrap_content.py
@@ -7,11 +7,4 @@ from Products.Five.browser import BrowserView
 
 
 class BootstrapContent(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('bootstrap-content.pt')
-
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    """Test the Bootstrap Content Section"""
