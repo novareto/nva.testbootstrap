128b94f04f4fef38bae94331d458c401f4fd4a1c
Lars Walther <meinkraeks@gmail.com>
2019-07-14 16:50:37 +0200

    Components fertiggestellt

--- src/nva/testbootstrap/views/bootstrap_components.py
+++ src/nva/testbootstrap/views/bootstrap_components.py
@@ -2,16 +2,14 @@
 
 from nva.testbootstrap import _
 from Products.Five.browser import BrowserView
+from plone import api as ploneapi
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class BootstrapComponents(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('bootstrap-components.pt')
+    """Test the Bootstrap Components"""
 
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    def static(self):
+        portal = ploneapi.portal.get().absolute_url()
+        return portal + '/++resource++nva.testbootstrap'
